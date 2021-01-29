9bde26af1e75fda5d867ea9c40d7f73890c1e4cf
Yuri Andrade <dev.yuri97@gmail.com>
2021-01-29 08:40:16 -0300

    atualizando cadastro do instrutor

--- instructors.js
+++ instructors.js
@@ -1,4 +1,31 @@
 const fs = require('fs');
+const data = require('./data.json');
+const { age } = require('./utils');
+
+// show
+exports.show = function(request, response) {
+    // request.params
+    const { id } = request.params;
+
+    const foundInstructor = data.instructors.find(function(instructor){
+        return instructor.id == id
+    });
+
+    if (!foundInstructor) {
+        return response.send('Instructor not found');
+    }
+
+
+    const instructor = {
+        // usando spread operator ( ... )
+        ...foundInstructor,
+        age: age(foundInstructor.birth),
+        services: foundInstructor.services.split(","),
+        created_at: new Intl.DateTimeFormat("pt-BR").format(foundInstructor.created_at),
+    }
+
+    return response.render("instructors/show", { instructor })
+}
 
 // create
 exports.post = function(request, response) {
@@ -12,7 +39,26 @@ exports.post = function(request, response) {
         }
     }
 
-    fs.writeFile("data.json", JSON.stringify(request.body), function() {
+    let {avatar_url, birth, name, services, gender} = request.body;
+
+    //manipulando datas
+    birth = Date.parse(birth);
+    const created_at = Date.now();
+    const id = Number(data.instructors.length + 1);
+    
+    
+
+    data.instructors.push({
+        id,
+        name,
+        avatar_url,
+        birth,
+        gender,
+        services,
+        created_at
+    });
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
         if(err) {
             return response.send("Write file error!");
         }
